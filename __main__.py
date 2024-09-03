d0ce79509f6ed702632265af6ea67a2778c87b92
scaryhomicide <rk661g@gmail.com>
2024-09-03 22:03:57 +0530

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,4 @@
+import MyGmail
+
+if __name__ == '__main__':
+    pass
---
+++ setup.py
@@ -0,0 +1,29 @@
+import re
+from setuptools import setup, find_packages
+
+setup(
+    name='MyGmail',
+    version="1",
+    url='https://github.com/homic1de/ezgmail-fork',
+    author='no',
+    author_email='z',
+    description="x",
+    long_description="x",
+    long_description_content_type="text/markdown",
+    test_suite='tests',
+    install_requires=['google-api-python-client', 'oauth2client'],
+    keywords='',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
